94ed40947c790a27ac8ce93ea813135312055499
Andrew Dron <Andrew.C.Dron@protonmail.com>
2023-06-17 15:31:49 -0600

    modify cache implementation to use traits

--- src/cache.rs
+++ src/cache.rs
@@ -2,29 +2,33 @@ use std::collections::HashMap;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
-pub struct Cache<T>
+trait Cache<T>
+{
+    fn new(_associativity: u64, _size: u64) -> Self;
+    fn lookup(&mut self, target: T) -> Option<&T>;
+}
+
+pub struct LruCache<'a, T>
+{
+    dict: HashMap<u64, Vec<&'a T>>,
+    associativity: u64,
+    size: u64,
+    num_sets: u64,
+}
+
+pub struct LFU_Cache<T>
 {
     dict: HashMap<u64, Vec<T>>,
     associativity: u64,
     size: u64,
     num_sets: u64,
-    policy: eviction_policy
 }
 
-#[derive(Default)]
-pub enum eviction_policy
+impl<'a, T: PartialEq + Hash> Cache<T> for LruCache<'a,T>
 {
-    #[default]
-    LRU,
-    MRU,
-    LFU
-}
-
-impl<T: PartialEq> Cache<T>
-{
-    pub fn new(_associativity: u64, _policy: eviction_policy, _size: u64) -> Self
+    fn new(_associativity: u64, _size: u64) -> Self
     {
-        let mut _dict : HashMap<u64, Vec<T>> = HashMap::new();
+        let mut _dict : HashMap<u64, Vec<&T>> = HashMap::new();
         let _num_sets = _size / _associativity;
 
         for i in 0.._num_sets
@@ -32,39 +36,30 @@ impl<T: PartialEq> Cache<T>
             _dict.insert(i, Vec::new());
         }
 
-        Cache
+        LruCache
         {
             dict: _dict,
             associativity: _associativity,
             size: _size,
             num_sets: _num_sets,
-            policy: _policy
         }
     }
 
-    pub fn lookup(&mut self, value: T) -> Option<T>
-    {
-        self.search(value)
-    }
-
-    fn find_set(&mut self, key: T) -> u64
+    fn lookup(&mut self, value: T) -> Option<&T>
     {
         let mut hasher = DefaultHasher::new();
-        key.hash(&mut key);
-        hasher.finish() % self.num_sets
-    }
+        value.hash(&mut hasher);
+        let loc  = hasher.finish() % self.num_sets;
 
-    fn search(&mut self, key: T) -> Option<T>
-    {
-        let mut set = self.dict.get_mut(&self.find_set(key)).unwrap();
+        let mut set = self.dict.get_mut(&loc).unwrap();
 
-        match set.iter().position(|&x| x == key)
+        match set.iter().position(|&x| *x == value)
         {
             Some(index) => 
             {
                 let val = set[index];
                 set.remove(index);
-                set.insert(0, val);
+                set.insert(0, &val);
                 Some(val)
             },
 
@@ -74,9 +69,10 @@ impl<T: PartialEq> Cache<T>
                 {
                     set.pop();
                 }
-                set.insert(0, key);
+                set.insert(0, &value);
                 None
             }
         }
     }
+
 }
